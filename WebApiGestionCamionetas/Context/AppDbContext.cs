fbc2aff0f9ff0c0e0ce24943fda0b872dfbdd409
JorgeUrra <jurra@alercechile.cl>
2020-02-13 19:18:24 -0300

    Subir API

---
+++ WebApiGestionCamionetas/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiGestionCamionetas.Entities;
+
+namespace WebApiGestionCamionetas.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
---
+++ WebApiGestionCamionetas/Controllers/UsuarioController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiGestionCamionetas.Context;
+using WebApiGestionCamionetas.Entities;
+
+namespace WebApiGestionCamionetas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        public UsuarioController(AppDbContext _context)
+        {
+            this.context = _context;
+        }
+        // GET: api/Usuario
+        [HttpGet]
+        public IEnumerable<Usuario> Get()
+        {
+            return context.Usuario.ToList();
+        }
+
+        // GET: api/Usuario/5
+        [HttpGet("{id}", Name = "Get")]
+        public Usuario Get(int id)
+        {
+            return context.Usuario.FirstOrDefault(u=>u.IdUsuario == id);
+        }
+
+        //// POST: api/Usuario
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT: api/Usuario/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
